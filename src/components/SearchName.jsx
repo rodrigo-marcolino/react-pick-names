b0891e7bc60af208ce460d8c3ce02bc5600d6f0e
Rodrigo Marcolino <rodrigomarcolino@gmail.com>
2022-01-28 16:17:28 +1300

    add useRef for input focus

--- src/components/SearchName.jsx
+++ src/components/SearchName.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 export const SearchName = () => {
   const [searchName, setSearchName] = useState('')
+  const inputRef = useRef()
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
   const handleChange = (e) => {
     setSearchName(e.target.value)
   }
   return (
     <header>
-      <input onChange={handleChange} type="text" placeholder="Type a name..." value={searchName} />
+      <input ref={inputRef} onChange={handleChange} type="text" placeholder="Type a name..." value={searchName} />
       <pre>Name: {searchName}</pre>
     </header>
   )
